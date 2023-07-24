bce0c62231d9a2f27a92a2ced987858cfe9b5046
Dmitry Programmer <dmproger@gmail.com>
2023-07-25 00:54:36 +0300

    litt-refact-formatted

--- app/models/concerns/formatted_datetime.rb
+++ app/models/concerns/formatted_datetime.rb
@@ -3,7 +3,7 @@ module FormattedDatetime
 
   def attributes
     result = super
-    result.merge(result.slice(*ATTRIBUTES).transform_values { |value| formatted(value) })
+    result.merge(result.slice(*ATTRIBUTES).transform_values { |value| value && formatted(value) })
   end
 
   def created_at
@@ -17,6 +17,6 @@ module FormattedDatetime
   private
 
   def formatted(time)
-    time&.iso8601
+    time.iso8601
   end
 end
