929d2f4bedacbfed9de33e4d2b1ea6644f4c5f9a
Dmitry Programmer <dmproger@gmail.com>
2023-07-26 17:19:52 +0300

    add-spec-for-timestamps-concern

---
+++ spec/concerns/timestamps_spec.rb
@@ -0,0 +1,52 @@
+RSpec.describe Timestamps do
+  Standard1 = Class.new do
+    def self.call(value)
+      "standard1 #{value}"
+    end
+  end
+  Standard2 = Class.new do
+    def self.call(value)
+      "standard2 #{value}"
+    end
+  end
+  Model = Class.new do
+    def attributes
+      { 'attr1' => 'attr1_value', 'created_at' => 'created_at_value' }
+    end
+
+    def [](attr)
+      { attr1: 'attr1_value', created_at: 'created_at_value' }[attr]
+    end
+  end
+  Model1 = Class.new(Model)
+  Model2 = Class.new(Model)
+  Model3 = Class.new(Model)
+
+  subject do
+    Model1.include(Timestamps[Standard1])
+    Model2.include(Timestamps[Standard1])
+    Model3.include(Timestamps[Standard2])
+  end
+
+  it 'has correct behavior' do
+    subject
+
+    [Model1.new, Model2.new].each do |model|
+      expect(model.created_at).to eq('standard1 created_at_value')
+      expect(model.updated_at).to eq('standard1 ')
+      expect(model.attributes).to match(
+        'attr1' => 'attr1_value',
+        'created_at' => 'standard1 created_at_value'
+      )
+    end
+
+    [Model3.new].each do |model|
+      expect(model.created_at).to eq('standard2 created_at_value')
+      expect(model.updated_at).to eq('standard2 ')
+      expect(model.attributes).to match(
+        'attr1' => 'attr1_value',
+        'created_at' => 'standard2 created_at_value'
+      )
+    end
+  end
+end
